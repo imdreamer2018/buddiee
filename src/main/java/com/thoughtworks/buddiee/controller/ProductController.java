444e50796bc949480b89c266041b0d30116ed52f
yangqian <qian.yang2@thoughtworks.com>
2020-11-11 15:24:01 +0800

    [yangqian] feat: create product controller and test.

--- src/main/java/com/thoughtworks/buddiee/controller/ProductController.java
+++ src/main/java/com/thoughtworks/buddiee/controller/ProductController.java
@@ -1,14 +1,23 @@
 package com.thoughtworks.buddiee.controller;
 
+import com.thoughtworks.buddiee.dto.Product;
 import com.thoughtworks.buddiee.service.ProductService;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(value = "products")
 public class ProductController {
 
     private final ProductService productService;
 
     public ProductController(ProductService productService) {
         this.productService = productService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product createProduct(@RequestBody Product product) {
+        return productService.createProduct(product);
     }
 }
--- src/test/java/com/thoughtworks/buddiee/controllerTests/ProductControllerTest.java
+++ src/test/java/com/thoughtworks/buddiee/controllerTests/ProductControllerTest.java
@@ -5,13 +5,23 @@ import com.thoughtworks.buddiee.dto.Product;
 import com.thoughtworks.buddiee.service.ProductService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ProductController.class)
 @AutoConfigureJsonTesters
@@ -26,13 +36,39 @@ public class ProductControllerTest {
     @Autowired
     private JacksonTester<Product> productJson;
 
+    private Product product;
+
     @BeforeEach
     void setUp() {
-
+        product = Product.builder()
+                .name("mock product name")
+                .description("mock product description")
+                .imageUrl("mock image url")
+                .price(10)
+                .build();
     }
 
     @AfterEach
     void afterEach() {
         Mockito.reset(productService);
+    }
+
+    @Nested
+    class CreateProduct {
+
+        @Nested
+        class WhenCreateSuccess {
+
+            @Test
+            void should_return_product_info() throws Exception {
+                when(productService.createProduct(product)).thenReturn(product);
+                mockMvc.perform(post("/products")
+                        .content(productJson.write(product).getJson())
+                        .contentType(MediaType.APPLICATION_JSON))
+                        .andExpect(status().isCreated())
+                        .andExpect(jsonPath("$.name", is("mock product name")));
+                verify(productService).createProduct(product);
+            }
+        }
     }
 }
