ce77c812cc3e910841915bb7e9bcf1369eb2e31b
yangqian <qian.yang2@thoughtworks.com>
2020-11-11 15:52:17 +0800

    [yangqian] feat: add bad request exception into global exception handler.

---
+++ src/main/java/com/thoughtworks/buddiee/exception/ErrorResult.java
@@ -0,0 +1,14 @@
+package com.thoughtworks.buddiee.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+class ErrorResult {
+    private String message;
+}
---
+++ src/main/java/com/thoughtworks/buddiee/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.thoughtworks.buddiee.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorResult> handle(BadRequestException e) {
+        ErrorResult errorResult = ErrorResult.builder()
+                .message(e.getMessage())
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResult);
+    }
+
+}
