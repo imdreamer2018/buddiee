358e70e8d579c9a0613c78bda7be6aadffe3aed6
yangqian <qian.yang2@thoughtworks.com>
2020-11-11 15:23:48 +0800

    [yangqian] feat: create product service and test.

--- src/main/java/com/thoughtworks/buddiee/service/ProductService.java
+++ src/main/java/com/thoughtworks/buddiee/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.buddiee.service;
 
+import com.thoughtworks.buddiee.dto.Product;
 import com.thoughtworks.buddiee.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,5 +11,11 @@ public class ProductService {
 
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
+    }
+
+    public Product createProduct(Product product) {
+        Long productId = productRepository.save(product);
+        product.setId(productId);
+        return product;
     }
 }
--- src/test/java/com/thoughtworks/buddiee/serviceTests/ProductServiceTest.java
+++ src/test/java/com/thoughtworks/buddiee/serviceTests/ProductServiceTest.java
@@ -1,12 +1,16 @@
 package com.thoughtworks.buddiee.serviceTests;
 
+import com.thoughtworks.buddiee.dto.Product;
 import com.thoughtworks.buddiee.repository.ProductRepository;
 import com.thoughtworks.buddiee.service.ProductService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 @ExtendWith(MockitoExtension.class)
@@ -17,10 +21,32 @@ public class ProductServiceTest {
     @Mock
     ProductRepository productRepository;
 
+    private Product product;
+
     @BeforeEach
     void setUp() {
         initMocks(this);
         productService = new ProductService(productRepository);
+        product = Product.builder()
+                .name("mock product name")
+                .description("mock product description")
+                .imageUrl("mock image url")
+                .price(10)
+                .build();
+    }
+
+    @Nested
+    class CreateProduct {
+
+        @Nested
+        class WhenCreateSuccess {
+
+            @Test
+            void should_return_product_info() {
+                Product productResponse = productService.createProduct(product);
+                assertEquals("mock product name", productResponse.getName());
+            }
+        }
     }
 
 }
