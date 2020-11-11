e1a576192319015ab064785a87280993cd4f48ff
yangqian <qian.yang2@thoughtworks.com>
2020-11-11 17:18:44 +0800

    [yangqian] remove: contextLoads.

--- src/test/java/com/thoughtworks/buddiee/BuddieeApplicationTests.java
+++ src/test/java/com/thoughtworks/buddiee/BuddieeApplicationTests.java
@@ -1,13 +1,8 @@
 package com.thoughtworks.buddiee;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BuddieeApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
