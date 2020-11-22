44a38e91d12d6c87f9bf22c38ee413833b658757
yangqian <qian.yang2@thoughtworks.com>
2020-11-22 21:50:20 +0800

    [yangqian] fix: product entity id type is long.

--- src/main/java/com/thoughtworks/buddiee/entity/ProductEntity.java
+++ src/main/java/com/thoughtworks/buddiee/entity/ProductEntity.java
@@ -22,7 +22,7 @@ public class ProductEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private Long id;
     private String name;
     private String description;
     private String imageUrl;
