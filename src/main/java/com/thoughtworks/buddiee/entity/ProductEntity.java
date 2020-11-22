aa36ba9e0c908965ed66fc50790433919403671d
yangqian <qian.yang2@thoughtworks.com>
2020-11-22 22:45:44 +0800

    [yangqian] fix: DynamicUpdate.

--- src/main/java/com/thoughtworks/buddiee/entity/ProductEntity.java
+++ src/main/java/com/thoughtworks/buddiee/entity/ProductEntity.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -18,6 +19,7 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@DynamicUpdate
 @Table(name = "product")
 public class ProductEntity {
 
