bad99bb13b8fa3e9f9fe7b9df5204a363afd6990
yangqian <qian.yang2@thoughtworks.com>
2020-11-25 15:33:29 +0800

    [yangqian] feat: product controller integration test and use rest-assured.

---
+++ src/test-integration/java/com.thoughtworks.buddiee/api/ProductControllerTest.java
@@ -0,0 +1,77 @@
+package com.thoughtworks.buddiee.api;
+
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import com.thoughtworks.buddiee.base.ApiBaseTest;
+import com.thoughtworks.buddiee.entity.ProductEntity;
+import com.thoughtworks.buddiee.repository.ProductRepository;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+
+import static com.toomuchcoding.jsonassert.JsonAssertion.assertThatJson;
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class ProductControllerTest extends ApiBaseTest {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @BeforeEach
+    void setUp() {
+        prepareProductData();
+    }
+
+    private void prepareProductData() {
+        productRepository.save(
+                ProductEntity.builder()
+                .id(1L)
+                .name("可乐")
+                .description("快乐水")
+                .imageUrl("http://####")
+                .price(new BigDecimal("2.5"))
+                .build()
+        );
+        productRepository.save(
+                ProductEntity.builder()
+                        .id(2L)
+                        .name("雪碧")
+                        .description("大声跟你逼逼")
+                        .imageUrl("http://####")
+                        .price(new BigDecimal("2.5"))
+                        .build()
+        );
+        productRepository.save(
+                ProductEntity.builder()
+                        .id(3L)
+                        .name("脉动")
+                        .description("快乐水")
+                        .imageUrl("http://####")
+                        .price(new BigDecimal("4"))
+                        .build()
+        );
+    }
+
+    @Nested
+    class FindById {
+
+        @Test
+        void should_return_product_info_when_product_existed() {
+            RequestSpecification request = given().header("Content-Type", "application/json");
+
+            Response response = given().spec(request)
+                    .get("products/1");
+
+            assertThat(response.statusCode()).isEqualTo(200);
+            DocumentContext parsedJson = JsonPath.parse(response.getBody().asString());
+            assertThatJson(parsedJson).field("['name']").isEqualTo("可乐");
+        }
+    }
+}
---
+++ src/test-integration/java/com.thoughtworks.buddiee/base/ApiBaseTest.java
@@ -0,0 +1,33 @@
+package com.thoughtworks.buddiee.base;
+
+import com.thoughtworks.buddiee.BuddieeApplication;
+import io.restassured.RestAssured;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.context.WebApplicationContext;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = BuddieeApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public abstract class ApiBaseTest {
+
+    @LocalServerPort
+    int port;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @BeforeEach
+    public void setup() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = this.port;
+        RestAssuredMockMvc.enableLoggingOfRequestAndResponseIfValidationFails();
+        RestAssuredMockMvc.webAppContextSetup(webApplicationContext);
+    }
+}
