5b261975cdf762abaf4bc570a5ba8d977a51de8c
yangqian <qian.yang2@thoughtworks.com>
2020-11-27 14:55:36 +0800

    [yangqian] fix: serialVersionUID.

--- src/main/java/com/thoughtworks/buddiee/exception/BadRequestException.java
+++ src/main/java/com/thoughtworks/buddiee/exception/BadRequestException.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.buddiee.exception;
 
 public class BadRequestException extends RuntimeException{
+    public static final long serialVersionUID = 4328743;
     public BadRequestException(String message) {
         super(message);
     }
--- src/main/java/com/thoughtworks/buddiee/exception/ResourceNotFoundException.java
+++ src/main/java/com/thoughtworks/buddiee/exception/ResourceNotFoundException.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.buddiee.exception;
 
 public class ResourceNotFoundException extends RuntimeException{
+    public static final long serialVersionUID = 4328744;
     public ResourceNotFoundException(String message) {
         super(message);
     }
