4f83c52a66e837be0ba0cab7a98522bdd71863e4
yangqian <qian.yang2@thoughtworks.com>
2020-12-07 14:34:13 +0800

    [yangqian] feat: user repository.

---
+++ src/main/java/com/thoughtworks/buddiee/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.thoughtworks.buddiee.repository;
+
+import com.thoughtworks.buddiee.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    Optional<User> findUserByUsername(String username);
+
+    Optional<User> findUserByEmail(String email);
+}
