fc80caf746714e17bf528a03437b49c1666f8b54
yangqian <qian.yang2@thoughtworks.com>
2020-12-08 14:33:40 +0800

    [yangqian] fix: add /api/auth/logout in spring SecurityConfiguration.

--- src/main/java/com/thoughtworks/buddiee/configuration/SecurityConfiguration.java
+++ src/main/java/com/thoughtworks/buddiee/configuration/SecurityConfiguration.java
@@ -51,7 +51,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.POST, API_PRODUCTS).authenticated()
+                .antMatchers(HttpMethod.POST, API_PRODUCTS, "/api/auth/logout").authenticated()
                 .antMatchers(HttpMethod.PUT, API_PRODUCTS + "/**").authenticated()
                 .antMatchers(HttpMethod.DELETE, API_PRODUCTS + "/**").authenticated()
                 .anyRequest().permitAll()
